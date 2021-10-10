c1ea3f48df45051584544f7a84de6ec935792791
songulija <65647664+songulija@users.noreply.github.com>
2021-10-10 16:05:15 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Header from './components/header-component/Header';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (//have to wrap entire App in Router in order to use it
     <Router>
       <Header />
       <main className='py-3'>
-      <Container>
-        <Route path='/' component={HomeScreen} exact />
-      </Container>
+        <Container>
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
       </main>
     </Router>
   );
---
+++ src/screens/CartScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CartScreen() {
+    return (
+        <div>
+            <h1>Heelo</h1>
+        </div>
+    )
+}
+
+export default CartScreen
