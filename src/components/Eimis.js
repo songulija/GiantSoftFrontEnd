2be0d33e5f5727cc609936dd99269c9091fe26df
songulija <65647664+songulija@users.noreply.github.com>
2021-10-10 16:37:07 +0300

    Added LoginScreen component

---
+++ src/components/Eimis.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Eimis() {
+    return (
+        <div>
+            <h2>Eimis big boss</h2>
+        </div>
+    )
+}
+
+export default Eimis
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,32 +1,44 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Eimis from '../components/Eimis'
 
 function LoginScreen() {
     return (
-        <div>
-            {/* <main class="form-signin">
-                <form>
-                    <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-                    <h1 class ="h3 mb-3 fw-normal">Please sign in</h1>
+        <>
+            <body class="text-center" cz-shortcut-listen="true">
+            <Container className="my-auto">
+                <img class="mb-4" src="https://i.pinimg.com/originals/89/7a/61/897a61f56ec18239c658bf02cdba1b6e.jpg" alt="" width="200" height="200" />
+            </Container>
+                <main class="form-signin">
+                    <form>
+                        {/* <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> */}
+                        <h1 class ="h3 mb-3 fw-normal">Please sign in</h1>
 
-                    <div class ="form-floating">
-                    <input type ="email" class ="form-control" id="floatingInput" placeholder="name@example.com">
-                    <label for="floatingInput">Email address</label>
-                    </div>
-                    <div class ="form-floating">
-                    <input type ="password" class ="form-control" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">Password</label>
-                    </div>
+                        <div class ="form-floating">
+                        <input type ="email" class ="form-control" id="floatingInput" placeholder="name@example.com"/>
+                        <label for="floatingInput">Email address</label>
+                        </div>
+                        <div class ="form-floating">
+                        <input type ="password" class ="form-control" id="floatingPassword" placeholder="Password"/>
+                        <label for="floatingPassword">Password</label>
+                        </div>
 
-                    <div class ="checkbox mb-3">
-                    <label>
-                    <input type ="checkbox" value="remember-me"> Remember me
-                    </label>
-                    </div>
-                    <button class ="w-100 btn btn-lg btn-primary" type ="submit">Sign in</button>
-                    <p class ="mt-5 mb-3 text-muted">© 2017–2021</p>
-                </form>
-            </main> */}
-        </div>
+                        <div class ="checkbox mb-3">
+                        <label>
+                        <input type ="checkbox" value="remember-me"/>Remember me
+                        </label>
+                        </div>
+                        <button class ="w-100 btn btn-lg btn-primary" type ="submit">Sign in</button>
+                        <p class ="mt-5 mb-3 text-muted">© 2017–2021</p>
+                    </form>
+                </main>
+
+
+
+
+
+            </body>
+        </>
     )
 }
 
