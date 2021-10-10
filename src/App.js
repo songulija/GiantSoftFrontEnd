a64552e3d34e88de91668fc1e76bc97c5ef677ea
songulija <65647664+songulija@users.noreply.github.com>
2021-10-10 15:55:08 +0300

    Added all needed screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Container} from 'react-bootstrap'
 import Header from './components/header-component/Header';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
-  return (
-    <div className="App">
+  return (//have to wrap entire App in Router in order to use it
+    <Router>
       <Header />
-    </div>
+      <main className='py-3'>
+      <Container>
+        <Route path='/' component={HomeScreen} exact />
+      </Container>
+      </main>
+    </Router>
   );
 }
 
